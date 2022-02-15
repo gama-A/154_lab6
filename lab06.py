5f1f309e99b6ea4f2efa6fcc2cde4fe23350f700
Gama Aristondo <gamaliel@cs.ucsb.edu>
2022-02-14 19:46:19 -0800

    ga - beginning part 2

---
+++ lab06.py
@@ -0,0 +1,22 @@
+import pyrtl
+
+# Register file creation
+rf = pyrtl.MemBlock(bitwidth=32, addrwidth=5, name='rf')
+
+data = pyrtl.WireVector(bitwidth=32, name='data')
+decoder = pyrtl.RomBlock(bitwidth=32, addrwidth=1, romdata=pyrtl.Input(32,name='instr'))
+
+data = decoder[0]
+
+funct = pyrtl.WireVector(bitwidth=6, name='funct')
+sh = pyrtl.WireVector(bitwidth=5, name='sh')
+rs = pyrtl.WireVector(bitwidth=5, name='rs')
+rt = pyrtl.WireVector(bitwidth=5, name='rt')
+rd = pyrtl.WireVector(bitwidth=5, name='rd')
+alu_out = pyrtl.WireVector(bitwidth=32, name='alu_out')
+
+funct <<= data[0:6]
+sh <<= data[6:10]
+rd <<= data[11:15]
+rt <<= data[16:21]
+rs <<= data[21:25]
