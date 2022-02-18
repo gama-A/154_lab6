57bc32164a7e3201950de7fc2ea2cc11c21fbe5c
Gama Aristondo <gamaliel@cs.ucsb.edu>
2022-02-17 22:57:55 -0800

    rename check

--- lab06.py
+++ lab06.py
@@ -16,6 +16,8 @@ rs = pyrtl.WireVector(bitwidth=5, name='rs')
 rt = pyrtl.WireVector(bitwidth=5, name='rt')
 rd = pyrtl.WireVector(bitwidth=5, name='rd')
 alu_out = pyrtl.WireVector(bitwidth=32, name='alu_out')
+data0 = pyrtl.WireVector(bitwidth=32, name='data0')
+data1 = pyrtl.WireVector(bitwidth=32, name='data1')
 
 funct <<= data[0:6]
 sh <<= data[6:11]
@@ -23,24 +25,27 @@ rs <<= data[21:26]
 rt <<= data[16:21]
 rd <<= data[11:16]
 
+data0 <<= rf[rs]
+data1 <<=rf[rt]
+
 with pyrtl.conditional_assignment:
     with funct == 32:
-        alu_out |= rf[rs] + rf[rt]
+        alu_out |= data0 + data1
     with funct == 34:
-        alu_out |= rf[rs] + rf[rt]
+        alu_out |= data0 - data1
     with funct == 36:
-        alu_out |= rf[rs] & rf[rt]
+        alu_out |= data0 & data1
     with funct == 37:
-        alu_out |= rf[rs] | rf[rt]
+        alu_out |= data0 | data1
     with funct == 38:
-        alu_out |= rf[rs] ^ rf[rt]
+        alu_out |= data0 ^ data1
     with funct == 0:
-        alu_out |= pyrtl.shift_left_logical(rf[rt], sh)
+        alu_out |= pyrtl.shift_left_logical(data1, sh)
     with funct == 2:
-        alu_out |= pyrtl.shift_right_logical(rf[rt], sh)
+        alu_out |= pyrtl.shift_right_logical(data1, sh)
     with funct == 3:
-        alu_out |= pyrtl.shift_right_arithmetic(rf[rt], sh)
+        alu_out |= pyrtl.shift_right_arithmetic(data1, sh)
     with funct == 42:
-        alu_out |= rf[rs] < rf[rt]
+        alu_out |= data0 < data1
 
 rf[rd] <<= alu_out
